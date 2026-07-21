dc6076ca602cd3df32336b509b97bb640269592a
loiturong <loitruong2345@gmail.com>
2026-07-21 23:38:55 +0700

    feat: zero bss function

--- startup.s
+++ startup.s
@@ -6,7 +6,40 @@
 
 reset_handler:
 	ldr sp, =_sstack_top
+
+	/* zero bss and copy data */
+	bl zero_bss
+
 	bl kernel_main
+.main_literal_pool:
+	/* literal pool */
+	.ltorg
+
+/*
+ * Zero-out bss section
+ */
+zero_bss:
+	push {r4, r5, r6, r7}
+	
+	ldr r4, =bss_load_start
+	ldr r5, =bss_size
+	cmp r5, 0x00
+	beq .bss_ret
+
+	mov r6, 0x00
+	mov r7, 0x00
+.bss_loop:
+	/* str store a word = 4 bytes */
+	str r7, [r4, r6]
+	add r6, 0x04
+	cmp r5, r6
+	bpl .bss_loop
+.bss_ret:
+	pop {r4, r5, r6, r7}
+	mov pc, lr
+.bss_literal_pool:
+	/* literal pool */
+	.ltorg
 
 .section .hardware_startup
 .type  int_table, %object
