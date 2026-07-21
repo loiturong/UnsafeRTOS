153992badced7f7455acfd31dab3d620504104c4
loiturong <loitruong2345@gmail.com>
2026-07-21 23:56:48 +0700

    feat: merge bss and copy data into one function

--- startup.s
+++ startup.s
@@ -8,7 +8,7 @@ reset_handler:
 	ldr sp, =_sstack_top
 
 	/* zero bss and copy data */
-	bl zero_bss
+	bl init_data
 
 	bl kernel_main
 .main_literal_pool:
@@ -18,14 +18,15 @@ reset_handler:
 /*
  * Zero-out bss section
  */
-zero_bss:
+init_data:
 	push {r4, r5, r6, r7}
-	
+
+/* Zero out bss region on RAM */
+.bss_zero:
 	ldr r4, =bss_load_start
 	ldr r5, =bss_size
 	cmp r5, 0x00
-	beq .bss_ret
-
+	beq .data_copy
 	mov r6, 0x00
 	mov r7, 0x00
 .bss_loop:
@@ -34,10 +35,27 @@ zero_bss:
 	add r6, 0x04
 	cmp r5, r6
 	bpl .bss_loop
-.bss_ret:
+
+/* Copy data region to RAM */
+.data_copy:
+	ldr r4, =data_load_start
+	ldr r5, =data_start
+	ldr r6, =data_size
+	cmp r6, 0x00
+	beq .ret
+	mov r7, 0x00
+.data_loop:
+	/* str store a word = 4 bytes */
+	ldr r8, [r4, r7]
+	str r8, [r5, r7]
+	add r7, 0x04
+	cmp r6, r7
+	bpl .data_loop
+
+.ret:
 	pop {r4, r5, r6, r7}
 	mov pc, lr
-.bss_literal_pool:
+.data_bss_literal_pool:
 	/* literal pool */
 	.ltorg
 
