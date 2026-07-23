b61a17f57384cb0b2bff47577d7dec843e4489ac
loiturong <loitruong2345@gmail.com>
2026-07-23 16:23:16 +0700

    feat: SysTick_config
    just a copy from CMSIS, changes may be conducted latter.

---
+++ port/port.c
@@ -0,0 +1,33 @@
+/**
+ * @file    : port.c
+ * @brief   : ARM specific
+ *
+ * @Author  : Loiturong
+ * @License : GNU GENERAL PUBLIC LICENSE
+ */
+
+#include <CMSIS/DeviceHeader/stm32f405xx.h>
+
+/*
+ * SysTick configuration
+ * Now assume using HSI
+ * interval in miliseconnd
+ */
+uint32_t SysTick_Config(uint32_t freq, uint32_t interval)
+{	
+	// Only has [23:0] usable, [32:24] is reserved
+	if ((ticks - 1) > SysTick_LOAD_RELOAD_Msk)
+	{
+	  return 1;
+	}
+
+	SysTick->LOAD = (freq - 1)/ interval;
+	SysTick->VAL = 0;
+	
+	// internal clock source + enable interrupt + enable counter
+	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk |
+		SysTick_CTRL_TICKINT_Msk |
+		SysTick_CTRL_ENABLE_Msk;
+	return 0;
+}
+
---
+++ port/port.h
@@ -0,0 +1,14 @@
+/**
+ * @file    : port.h
+ * @brief   : 
+ *
+ * @Author  : Loiturong
+ * @License : GNU GENERAL PUBLIC LICENSE
+ */
+
+#ifndef PORT_H
+#define PORT_H
+
+uint32_t SysTick_Config(uint32_t freq, uint32_t interval)
+
+#endif /* PORT_H */
