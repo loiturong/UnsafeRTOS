755c8d0b29e6449c6d22b2e046320f0e8199d04b
loiturong <loitruong2345@gmail.com>
2026-08-04 16:08:07 +0700

    feat: External Interrupt in the vector table
    Also, split exception and external interrupt into 2 sections, one in
    Port layer, other is in HAL layer

--- HAL/vector_table.s
+++ HAL/vector_table.s
@@ -1,55 +1,221 @@
 .syntax unified
 
-.section .text.exceptions
-/* Default execption handler with infinite loop */
-Default_Handler:
-	b Default_Handler
-
-.weak MNI_Handler
-.weak HardFault_Handler
-.weak MemManage_Handler
-.weak BusFault_Handler
-.weak UsageFault_Handler
-.weak SVCall_Handler
-.weak DebugMonitor_Handler
-.weak PendSV_Handler
-.weak SysTick_Handler
-
-/* Temporary */
-.thumb_set MNI_Handler, Default_Handler
-.thumb_set HardFault_Handler, Default_Handler
-.thumb_set MemManage_Handler, Default_Handler
-.thumb_set BusFault_Handler, Default_Handler
-.thumb_set UsageFault_Handler, Default_Handler
-.thumb_set SVCall_Handler, Default_Handler
-.thumb_set DebugMonitor_Handler, Default_Handler
-.thumb_set PendSV_Handler, Default_Handler
-.thumb_set SysTick_Handler, Default_Handler
-
+/*
+ * This Interurpt vector table is based on RM0383 Reference manual for F411
+ * TODO: Find some way to upgrade HAL build system to handle multiple board support (latter)
+*/
 .section .interrupt_vector_table, "a", %progbits
-.type  vector_table, %object
-.global vector_table
+.type  external_interrupt_vector_table, %object
+.global external_interrupt_vector_table
 
-/* ARM fetch first 2 word for its hardware startup sequence */
-/* also place the interrupt vector table in this region */
-.vector_table:
-	.word _sstack_top
-	.word Reset_Handler
-	.word MNI_Handler
-	.word HardFault_Handler
-	.word MemManage_Handler
-	.word BusFault_Handler
-	.word UsageFault_Handler
-	.word 0x00
-	.word 0x00
-	.word 0x00
-	.word 0x00
-	.word SVCall_Handler
-	.word DebugMonitor_Handler
-	.word 0x0
-	.word PendSV_Handler
-	.word SysTick_Handler
-	/* Other added latter */
-	.rept 240
-	.word Default_Handler
-	.endr
+/* External Interrupt */
+.external_interrupt_vector_table:
+	.word WWDG_Handler
+	.word EXTI16_Handler
+	.word EXTI21_Handler
+	.word EXTI22_Handler
+	.word FLASH_INT_Handler
+	.word RCC_INT_Handler
+	.word EXTI0_Handler
+	.word EXTI1_Handler
+	.word EXTI2_Handler
+	.word EXTI3_Handler
+	.word EXTI4_Handler
+	
+	.word DMA1_Stream0_INT_Handler
+	.word DMA1_Stream1_INT_Handler
+	.word DMA1_Stream2_INT_Handler
+	.word DMA1_Stream3_INT_Handler
+	.word DMA1_Stream4_INT_Handler
+	.word DMA1_Stream5_INT_Handler
+	.word DMA1_Stream6_INT_Handler
+	.word ADC_INT_Handler
+	.word EXTI9_5_INT_Handler
+	
+	.word TIM1_BRK_TIM9_INT_Handler
+	.word TIM1_UP_TIM10_INT_Handler
+	.word TIM1_CC_INT_Handler
+	.word ITM2_INT_Handler
+	.word ITM3_INT_Handler
+	.word ITM4_INT_Handler
+	
+	.word I2C1_EV_INT_Handler
+	.word I2C1_ER_INT_Handler
+	.word I2C2_EV_INT_Handler
+	.word I2C2_ER_INT_Handler
+	
+	.word SPI1_INT_Handler
+	.word SPI2_INT_Handler
+
+	.word USART1_INT_Handler
+	.word USART2_INT_Handler
+
+	.word EXTI5_10_Handler
+	.word EXTI17_Handler
+	.word EXTI18_Handler
+
+	.word DMA1_Stream7_INT_Handler
+
+	.word SDIO_INT_Handler
+	.word TIM5_INT_Handler
+	.word SPI3_INT_Handler
+
+	.word DMA2_Stream0_INT_Handler
+	.word DMA2_Stream1_INT_Handler
+	.word DMA2_Stream2_INT_Handler
+	.word DMA2_Stream3_INT_Handler
+	.word DMA2_Stream4_INT_Handler
+
+	.word OTG_FS_INT_Handler
+
+	.word DMA2_Stream5_INT_Handler
+	.word DMA2_Stream6_INT_Handler
+	.word DMA2_Stream7_INT_Handler
+
+	.word USART6_INT_Handler
+	.word I2C3_EV_INT_Handler
+	.word I2C3_ER_INT_Handler
+
+	.word FPU_INT_Handler
+	.word SPI4_INT_Handler
+	.word SPI5_INT_Handler
+
+.section .text.exceptions
+/* Default interrupt handler
+ * This function "branching" EXEC_RETURN value into PC register, 
+ * which will cause an exception return, and pop the stack frame from which the cpu enterd.
+ * TODO: replace this function with a macro that handle clear IRQ source, 
+ * or handle clear IRQ source inside this function.
+ */
+Default_Interrupt_Handler:
+	bx lr
+
+.weak WWDG_Handler
+.weak EXTI16_Handler
+.weak EXTI21_Handler
+.weak EXTI22_Handler
+.weak FLASH_INT_Handler
+.weak RCC_INT_Handler
+.weak EXTI0_Handler
+.weak EXTI1_Handler
+.weak EXTI2_Handler
+.weak EXTI3_Handler
+.weak EXTI4_Handler
+
+.weak DMA1_Stream0_INT_Handler
+.weak DMA1_Stream1_INT_Handler
+.weak DMA1_Stream2_INT_Handler
+.weak DMA1_Stream3_INT_Handler
+.weak DMA1_Stream4_INT_Handler
+.weak DMA1_Stream5_INT_Handler
+.weak DMA1_Stream6_INT_Handler
+
+.weak ADC_INT_Handler
+.weak EXTI9_5_INT_Handler
+
+.weak TIM1_BRK_TIM9_INT_Handler
+.weak TIM1_UP_TIM10_INT_Handler
+.weak TIM1_CC_INT_Handler
+.weak ITM2_INT_Handler
+.weak ITM3_INT_Handler
+.weak ITM4_INT_Handler
+
+.weak I2C1_EV_INT_Handler
+.weak I2C1_ER_INT_Handler
+.weak I2C2_EV_INT_Handler
+.weak I2C2_ER_INT_Handler
+
+.weak SPI1_INT_Handler
+.weak SPI2_INT_Handler
+
+.weak USART1_INT_Handler
+.weak USART2_INT_Handler
+
+.weak EXTI5_10_Handler
+.weak EXTI17_Handler
+.weak EXTI18_Handler
+
+.weak DMA1_Stream7_INT_Handler
+
+.weak SDIO_INT_Handler
+.weak TIM5_INT_Handler
+.weak SPI3_INT_Handler
+
+.weak DMA2_Stream0_INT_Handler
+.weak DMA2_Stream1_INT_Handler
+.weak DMA2_Stream2_INT_Handler
+.weak DMA2_Stream3_INT_Handler
+.weak DMA2_Stream4_INT_Handler
+
+.weak OTG_FS_INT_Handler
+
+.weak DMA2_Stream5_INT_Handler
+.weak DMA2_Stream6_INT_Handler
+.weak DMA2_Stream7_INT_Handler
+
+.weak USART6_INT_Handler
+.weak I2C3_EV_INT_Handler
+.weak I2C3_ER_INT_Handler
+
+.weak FPU_INT_Handler
+.weak SPI4_INT_Handler
+.weak SPI5_INT_Handler
+
+
+/* Unimplement Interrupt got linked to Default_Interrupt_Handler*/
+.thumb_set WWDG_Handler, Default_Interrupt_Handler
+.thumb_set EXTI16_Handler, Default_Interrupt_Handler
+.thumb_set EXTI21_Handler, Default_Interrupt_Handler
+.thumb_set EXTI22_Handler, Default_Interrupt_Handler
+.thumb_set FLASH_INT_Handler, Default_Interrupt_Handler
+.thumb_set RCC_INT_Handler, Default_Interrupt_Handler
+.thumb_set EXTI0_Handler, Default_Interrupt_Handler
+.thumb_set EXTI1_Handler, Default_Interrupt_Handler
+.thumb_set EXTI2_Handler, Default_Interrupt_Handler
+.thumb_set EXTI3_Handler, Default_Interrupt_Handler
+.thumb_set EXTI4_Handler, Default_Interrupt_Handler
+.thumb_set DMA1_Stream0_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA1_Stream1_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA1_Stream2_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA1_Stream3_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA1_Stream4_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA1_Stream5_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA1_Stream6_INT_Handler, Default_Interrupt_Handler
+.thumb_set ADC_INT_Handler, Default_Interrupt_Handler
+.thumb_set EXTI9_5_INT_Handler, Default_Interrupt_Handler
+.thumb_set TIM1_BRK_TIM10_INT_Handler, Default_Interrupt_Handler
+.thumb_set TIM1_UP_TIM11_INT_Handler, Default_Interrupt_Handler
+.thumb_set TIM1_CC_INT_Handler, Default_Interrupt_Handler
+.thumb_set ITM2_INT_Handler, Default_Interrupt_Handler
+.thumb_set ITM3_INT_Handler, Default_Interrupt_Handler
+.thumb_set ITM4_INT_Handler, Default_Interrupt_Handler
+.thumb_set I2C1_EV_INT_Handler, Default_Interrupt_Handler
+.thumb_set I2C1_ER_INT_Handler, Default_Interrupt_Handler
+.thumb_set I2C2_EV_INT_Handler, Default_Interrupt_Handler
+.thumb_set I2C2_ER_INT_Handler, Default_Interrupt_Handler
+.thumb_set SPI1_INT_Handler, Default_Interrupt_Handler
+.thumb_set SPI2_INT_Handler, Default_Interrupt_Handler
+.thumb_set USART1_INT_Handler, Default_Interrupt_Handler
+.thumb_set USART2_INT_Handler, Default_Interrupt_Handler
+.thumb_set EXTI5_10_Handler, Default_Interrupt_Handler
+.thumb_set EXTI17_Handler, Default_Interrupt_Handler
+.thumb_set EXTI18_Handler, Default_Interrupt_Handler
+.thumb_set DMA1_Stream7_INT_Handler, Default_Interrupt_Handler
+.thumb_set SDIO_INT_Handler, Default_Interrupt_Handler
+.thumb_set TIM5_INT_Handler, Default_Interrupt_Handler
+.thumb_set SPI3_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA2_Stream0_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA2_Stream1_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA2_Stream2_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA2_Stream3_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA2_Stream4_INT_Handler, Default_Interrupt_Handler
+.thumb_set OTG_FS_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA2_Stream5_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA2_Stream6_INT_Handler, Default_Interrupt_Handler
+.thumb_set DMA2_Stream7_INT_Handler, Default_Interrupt_Handler
+.thumb_set USART6_INT_Handler, Default_Interrupt_Handler
+.thumb_set I2C3_EV_INT_Handler, Default_Interrupt_Handler
+.thumb_set I2C3_ER_INT_Handler, Default_Interrupt_Handler
+.thumb_set FPU_INT_Handler, Default_Interrupt_Handler
+.thumb_set SPI4_INT_Handler, Default_Interrupt_Handler
+.thumb_set SPI5_INT_Handler, Default_Interrupt_Handler
