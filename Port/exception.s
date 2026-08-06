c23480af7011766bb600bb0eaf3082b80b482a0b
loiturong <loitruong2345@gmail.com>
2026-08-06 14:57:30 +0700

    fix: function without type in assembler
    assembler will not put the correct function address with thumb mode bit
    set without type, symbol, %function (keep forgeting this)

--- Port/exception.s
+++ Port/exception.s
@@ -2,9 +2,11 @@
 
 .section .text.exceptions
 .global PendSV_Handler
+.type PendSV_Handler, %function
 PendSV_Handler:
 	bx lr
 
 .global SVCall_Handler
+.type SVCall_Handler, %function
 SVCall_Handler:
 	bx lr 
