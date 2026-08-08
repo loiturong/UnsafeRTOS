83fd66168bc1615e3cd775b2e4cad6995ff14e4d
loiturong <loitruong2345@gmail.com>
2026-08-08 10:32:16 +0700

    docs: reasoning about PendSV handling
    though on PendSV Handler in Port/exception.s

--- Port/exception.s
+++ Port/exception.s
@@ -1,16 +1,10 @@
 .syntax unified
 
-.section .text.exceptions
-.global PendSV_Handler
-.type PendSV_Handler, %function
-PendSV_Handler:
-	bx lr
-
 .global SVCall_Handler
 .type SVCall_Handler, %function
 SVCall_Handler:
 	/* Save kernel context, don't know if need this yet */
-	push {R4-R11}
+	// push {R4-R11}
 	
 	ldr r0, =tasklisthead	// pointer to tasklist 
 	ldr r0, [r0]		// dereference pointer to tasklist (which is still a pointer) 
@@ -25,4 +19,24 @@ SVCall_Handler:
 	.balign 4
 .svcall_literal_pool:
 	.ltorg
+
+.section .text.exceptions
+.global PendSV_Handler
+.type PendSV_Handler, %function
+/* When PendSV is servered, we are always in the state of tail-chaining
+ * Which is: SysTick_Handler load EXEC_RETURN to PC, the core check for pending
+ * Exception, and if there is no other higher priority exception preempting, PendSV
+ * get serverd. The state of PendSV now is:
+ * - We are in Handler mode, using MSP as stack pointer
+ * - Context before SysTick is save to task PSP
+ * to save task context, we can not use push instruction. Instead, we must manually get
+ * the PSP from task, save r4-r11 to that pointer, using write operation to address.
+ * Then calling scheduler scheme to pick next task, which update tasklistcurrent pointer.
+ * Then do the same as SVCall_Handler, load this new stack to PSP, but before running 
+ * EXEC_RETURN, we need to now, pop r4-r11 from the stack first. 
+ * Which is now raising a problem to task initialization. We must init_context with R4-R11 for all
+ * Task, but the first task to be run by SVCall_Handler.
+ **/
+PendSV_Handler:
+	bx lr
 
