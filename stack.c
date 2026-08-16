af7e913db47708a7396f02f18875fe29a93662ec
loiturong <loitruong2345@gmail.com>
2026-08-16 14:21:54 +0700

    refactor: remove old stack.c code
    a struct for stack is just overhead.

--- stack.c
+++ stack.c
@@ -8,25 +8,6 @@
 
 #include "stack.h"
 
-void stack_from_array(stack_t *st, uintptr_t *array, size_t size)
-{
-	uintptr_t *top_stack = array + size;
-	st->top = top_stack;
-	st->pnt = top_stack;
-	st->size = size;
-	return;
-}
-
-void push_stack(stack_t *st, uintptr_t item)
-{
-	*(--st->pnt) = item;
-}
-
-uintptr_t pop_stack(stack_t *st)
-{
-	return *(st->pnt++);
-}
-
 /* 
  * Simplify stack operation by just providing the top stack,
  * Task will evaluate its stack size before use.
